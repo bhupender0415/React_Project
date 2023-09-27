7819960f63487aa5d95d8d9f7b77a62ad068f17e
Bhupender Singh <bhupender0415@gmail.com>
2023-09-27 14:46:37 +0530

    files

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,11 @@
+import Demo from './Demo.js'
+function App() {
+  return (
+    <>
+    <Demo/>
+    <h1>Welcome to react app Bhupender Singh</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/Demo.js
@@ -0,0 +1,9 @@
+
+// function name should be Capital in starting -> Demo
+function Demo(){
+    return(
+        <h1> Inside demo function </h1>
+    )
+}
+
+export default Demo;
